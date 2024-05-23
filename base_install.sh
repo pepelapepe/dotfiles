7092012396eebdc242ff833e9995d8825b5b62bd
pepelapepe <169191051+pepelapepe@users.noreply.github.com>
2024-05-24 00:30:39 +0200

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -11,12 +11,12 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
-pacman -S grub efibootmgr os-prober ntfs-3g gvfs gvfs-mtp base-devel linux-headers amd-ucode dhcpcd xdg-utils vim bash-completion reflector
+pacman -S grub efibootmgr os-prober ntfs-3g gvfs gvfs-mtp base-devel linux-headers amd-ucode networkmanager xdg-utils vim bash-completion reflector
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-systemctl enable dhcpcd.service
+systemctl enable NetworkManager.service
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 
