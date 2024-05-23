5f894b96116cd4cad708a8c4303737349a52f226
pepelapepe <169191051+pepelapepe@users.noreply.github.com>
2024-05-24 00:20:01 +0200

    Update hypr.sh

--- hypr.sh
+++ hypr.sh
@@ -3,6 +3,9 @@
 # Install hyprland dependencies
 yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang hyprcursor
 
+#Install Essentials
+yay -S kitty dolphin cliphist wofi xdg-desktop-portal-hyprland
+
 # Build Hyprland
 git clone --recursive https://github.com/hyprwm/Hyprland
 cd Hyprland
