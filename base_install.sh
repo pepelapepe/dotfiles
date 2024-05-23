125729007b08bfa80171dec36218b9fb37b44bd7
pepelapepe <169191051+pepelapepe@users.noreply.github.com>
2024-05-24 00:37:12 +0200

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -11,7 +11,7 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
-pacman -S grub efibootmgr os-prober ntfs-3g gvfs gvfs-mtp base-devel linux-headers amd-ucode networkmanager xdg-utils vim bash-completion reflector
+pacman -S grub efibootmgr os-prober ntfs-3g gvfs gvfs-mtp base-devel linux-headers amd-ucode networkmanager vim bash-completion reflector
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
